LiquidSmooth-Devices/android_device_samsung_d2spr

Fix Sprint eri file
Change-Id: Ied18fe639eaae494a05f01d74659625e68f72597

device.mk
@@ -22,3 +22,10 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/d2spr/overlay
 
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
+
+PRODUCT_COPY_FILES += \
+    device/samsung/d2bst/rootdir/etc/init.d/99d2bst:system/etc/init.d/99d2bst \
+    device/samsung/d2bst/rootdir/data/apns-conf.xml:system/apns-conf.xml \
+    device/samsung/d2bst/rootdir/data/preferred-apn1.xml:system/preferred-apn1.xml \
+    device/samsung/d2bst/rootdir/etc/eri-boost.xml:system/etc/eri-boost.xml \
+    device/samsung/d2bst/rootdir/data/d2bst-firstboot.sh:system/d2bst-firstboot.sh

RadonX-ROM/device_samsung_d2spr

device.mk
@@ -22,3 +22,8 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/d2spr/overlay
 
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
+
+PRODUCT_COPY_FILES += \
+    device/samsung/d2spr/rootdir/etc/init.d/99d2spr:system/etc/init.d/99d2spr \
+    device/samsung/d2spr/rootdir/etc/eri-sprint.xml:system/etc/eri-sprint.xml \
+    device/samsung/d2spr/rootdir/data/d2spr-firstboot.sh:system/d2spr-firstboot.sh